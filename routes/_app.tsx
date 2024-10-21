d57688e01ada469d1878d0ef6c22571b741909be
mit-27 <meetsuthar64@gmail.com>
2024-10-21 00:03:13 -0600

    Fix SEO

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -11,25 +11,42 @@ export default function App({ Component }: PageProps) {
           content={"Shadcn buttons for Deno's Fresh and Preact based framework."}
         />
         <meta name="keywords" content={"shadcn, fresh, preact, deno, button"} />
+
+        <meta property="og:title" content="Fresh Shadcn Buttons" />
+        <meta
+          property="og:description"
+          content="Shadcn buttons for Deno's Fresh and Preact based framework."
+        />
+        <meta
+          property="og:url"
+          content="https://fresh-shadcn-buttons.deno.dev/"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1934" />
+        <meta property="og:image:height" content="1176" />
+        <meta property="og:type" content="website" />
         <meta
           property="og:image"
-          content={"https://fresh-shadcn-buttons.deno.dev/og-image.webp"}
+          content={"https://fresh-shadcn-buttons.deno.dev/og-image.png"}
         />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Fresh Shadcn Buttons" />
+        <meta
+          name="twitter:description"
+          content="Shadcn buttons for Deno's Fresh and Preact based framework."
+        />
+        <meta name="twitter:image:type" content="image/png" />
+        <meta name="twitter:image:width" content="1934" />
+        <meta name="twitter:image:height" content="1176" />
+
         <meta
           name="twitter:image"
           content={"https://fresh-shadcn-buttons.deno.dev/og-image.webp"}
         />
+
         <meta name="author" content={"Mit Suthar"} />
-        <meta property="og:title" content={"Fresh Shadcn Buttons"} />
-        <meta
-          property="og:description"
-          content={"Shadcn buttons for Deno's Fresh and Preact based framework."}
-        />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content={"https://fresh-shadcn-buttons.deno.dev/"}
-        />
+
         <title>Fresh Shadcn Buttons</title>
         <link
           rel="preload"
