f11f10b227acaafb660ddb77a2ebdbd528f517ff
mit-27 <meetsuthar64@gmail.com>
2024-10-20 18:41:32 -0600

    Fix 404 Page and add SEO

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,6 +5,16 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={"Shadcn buttons for Deno's Fresh."} />
+        <meta name="keywords" content={"shadcn, fresh, preact, deno, button"} />
+        <meta name="author" content={"Mit Suthar"} />
+        <meta property="og:title" content={"Shadcn Fresh Buttons"} />
+        <meta
+          property="og:description"
+          content={"Shadcn buttons for Deno's Fresh."}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={""} />
         <title>fresh-shadcn-buttons</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
