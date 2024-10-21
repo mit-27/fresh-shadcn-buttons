798810cf246fc5337240b5bb0778ecacafddf08e
mit-27 <meetsuthar64@gmail.com>
2024-10-20 18:45:51 -0600

    Add og image

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { type PageProps } from "$fresh/server.ts";
+import { asset } from "$fresh/runtime.ts";
 export default function App({ Component }: PageProps) {
   return (
     <html>
@@ -7,6 +8,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={"Shadcn buttons for Deno's Fresh."} />
         <meta name="keywords" content={"shadcn, fresh, preact, deno, button"} />
+        <meta property="og:image" content={asset("og-image.webp")} />
         <meta name="author" content={"Mit Suthar"} />
         <meta property="og:title" content={"Shadcn Fresh Buttons"} />
         <meta
@@ -16,6 +18,13 @@ export default function App({ Component }: PageProps) {
         <meta property="og:type" content="website" />
         <meta property="og:url" content={""} />
         <title>fresh-shadcn-buttons</title>
+        <link
+          rel="preload"
+          href={asset("/fonts/FixelVariable.woff2")}
+          as="font"
+          type="font/woff2"
+          crossorigin="true"
+        />
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
